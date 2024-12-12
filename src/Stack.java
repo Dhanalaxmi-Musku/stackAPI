5f003e47d6046f36626fd633a7b5a45ac097ea74
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-12 21:57:43 +0530

    Added:Ability to peak and pop from the Stack

--- src/Stack.java
+++ src/Stack.java
@@ -9,6 +9,23 @@ public class Stack <T>{
     public int size() {
         return list.size();
     }
+    public T pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+        }
+        return list.pop();
+    }
+    public T peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+        }
+        T topItem = list.pop();  
+        list.add(topItem);       
+        return topItem;
+    }
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
     @Override
     public String toString() {
         return list.toString();
--- src/StackMain.java
+++ src/StackMain.java
@@ -7,6 +7,11 @@ public class StackMain {
 	        stack.push(30);
 	        stack.push(56);
 	        System.out.println("Stack: " + stack);
+	        System.out.println("Top element: " + stack.peek());
+	        System.out.println("Stack size: " + stack.size());
+	        System.out.println("Popped: " + stack.pop());
+	        System.out.println("Popped: " + stack.pop());
+	        System.out.println("Is stack empty? " + stack.isEmpty());
 
 	}
 
